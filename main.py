70da5c56c5b03f969bd7e5f4ea92a6d7aa86e4e5
fatt8312 <115389231+fatt8312@users.noreply.github.com>
2023-03-27 11:53:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
-from routes.courses_routes import course_api_router
+from motorcycle_shop import calculate_monthly_payment, calculate_total_payment
 
-app = FastAPI()
+def calculate_monthly_payment(price, down_payment):
+    remaining_balance = price - down_payment
+    monthly_interest_rate = 0.05 / 12
+    num_payments = 12
+    monthly_payment = remaining_balance * (monthly_interest_rate * (1 + monthly_interest_rate)**num_payments) / ((1 + monthly_interest_rate)**num_payments - 1)
+    return monthly_payment
 
-app.include_router(course_api_router)
+print(f"Monthly payment: {monthly_payment}")
+print(f"Total payment: {total_payment}")
